4392747851253fd6f379b71fa1954f8cdfe5f96d
tamarabanighanem <tbanighanem@gmail.com>
2023-07-03 22:03:12 -0700

    part 2

--- bank-app/src/Main.js
+++ bank-app/src/Main.js
@@ -1,32 +1,45 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import {  deleteAccount } from './store';
+import AccountForm from './AccountForm';
 const Main = () => {
-  const accounts = useSelector(state => state.accounts);
+  const accounts = useSelector((state) => state.accounts);
+  const dispatch = useDispatch();
+
+  const handleDelete = (accountId) => {
+    dispatch(deleteAccount(accountId));
+  };
+  console.log(accounts)
+  console.log(accounts)
+  console.log(accounts)
+  console.log(accounts)
 
   return (
     <div className="p-4 flex-grow">
-    <table className="w-full border-collapse">
-      <thead>
-        <tr>
-          <th className="py-2 px-4 bg-gray-100 border-b">ID</th>
-          <th className="py-2 px-4 bg-gray-100 border-b">Customer Name</th>
-          <th className="py-2 px-4 bg-gray-100 border-b">Account Number</th>
-          <th className="py-2 px-4 bg-gray-100 border-b">Account Type</th>
-        </tr>
-      </thead>
-      <tbody>
-        {accounts.map(account => (
-          <tr key={account.id} className="bg-white border-b">
-            <td className="py-2 px-4">{account.id}</td>
-            <td className="py-2 px-4">{account.customerName}</td>
-            <td className="py-2 px-4">{account.accountNumber}</td>
-            <td className="py-2 px-4">{account.accountType}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+      {/* Table code */}
+      <table className="w-full border-collapse">
+        {/* Table headers */}
+        <tbody>
+          {accounts.map((account) => (
+            <tr key={account.id} className="bg-white border-b">
+              <td className="py-2 px-4">{account.id}</td>
+              <td className="py-2 px-4">{account.customerName}</td>
+              <td className="py-2 px-4">{account.accountNumber}</td>
+              <td className="py-2 px-4">{account.accountType}</td>
+              <td className="py-2 px-4">
+                <button
+                  onClick={() => handleDelete(account.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <AccountForm />
+    </div>
   );
 };
 
--- bank-app/src/store.js
+++ bank-app/src/store.js
@@ -1,5 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 
+// Step 1: Define the initial state
 const initialState = {
   accounts: [
     {
@@ -13,27 +16,63 @@ const initialState = {
       customerName: "Ahmad Zahran",
       accountNumber: "987654",
       accountType: "Student accounts"
-    },
-    {
-      id: 3,
-      customerName: "Ahmad Zahran",
-      accountNumber: "987654",
-      accountType: "Student accounts"
-    },
-    {
-      id: 4,
-      customerName: "Ahmad Zahran",
-      accountNumber: "987654",
-      accountType: "Student accounts"
     }
-  ]
+  ],
+  numberOfAccounts: 2
+};
+// Step 2: Define action types
+const ADD_ACCOUNT = 'ADD_ACCOUNT';
+const DELETE_ACCOUNT = 'DELETE_ACCOUNT';
+
+// Step 3: Define async action using createAsyncThunk
+const fetchAccountData = createAsyncThunk(
+  'accounts/fetchAccountData',
+  async () => {
+    const response = await fetch('https://dummyjson.com/products');
+    const data = await response.json();
+
+    return data;
+
+  }
+  
+);
+
+// Step 4: Define action creators
+const addAccount = (account) => {
+  return {
+    type: ADD_ACCOUNT,
+    payload: account
+  };
 };
 
+const deleteAccount = (accountId) => {
+  return {
+    type: DELETE_ACCOUNT,
+    payload: accountId
+  };
+};
+
+// Step 5: Define the reducer
 const reducer = (state = initialState, action) => {
-  // You can add logic here to handle different actions if needed
-  return state;
+  switch (action.type) {
+    case ADD_ACCOUNT:
+      return {
+        ...state,
+        accounts: [...state.accounts, action.payload],
+        numberOfAccounts: state.numberOfAccounts + 1
+      };
+    case DELETE_ACCOUNT:
+      return {
+        ...state,
+        accounts: state.accounts.filter((account) => account.id !== action.payload),
+        numberOfAccounts: state.numberOfAccounts - 1
+      };
+    default:
+      return state;
+  }
 };
 
-const store = createStore(reducer);
+// Step 6: Create the Redux store
+const store = createStore(reducer, applyMiddleware(thunk));
 
-export default store;
+export { fetchAccountData, addAccount, deleteAccount, store };
